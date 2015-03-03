6192d848a197581689fcd3af2780f37de7c03018
denzelr <ryan.denzel@colorado.edu>
2015-03-02 18:32:48 -0700

    one is done

--- HW3-StarterCode/Problem1/SingletonQueue.java
+++ HW3-StarterCode/Problem1/SingletonQueue.java
@@ -4,18 +4,22 @@ import java.util.LinkedList;
 
 public class SingletonQueue{
 	private static SingletonQueue instance;
+	public static int numQueues = 0;
 	Queue<Submission> q;
 	private SingletonQueue(){
+		System.out.println("queue initialized");
 		q = new LinkedList<Submission>();
 	}
 	public static SingletonQueue getInstance(){
 		if(instance == null){
+			numQueues++;
 			instance = new SingletonQueue();
 		}
 		return instance;
 	}
 	public void add(Submission submission){
 		q.add(submission);
+		System.out.println(q);
 	}
 	public Submission process(){
 		Submission submission = q.poll();
---
+++ HW3-StarterCode/Problem2/Observer.java
@@ -0,0 +1,5 @@
+public class Observer {
+	public update(){
+		
+	}
+}
