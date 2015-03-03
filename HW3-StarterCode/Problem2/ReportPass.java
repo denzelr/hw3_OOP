52e6228033080cf3e72363f052a1e7772645bfa0
mbinsfeld <mbinsfeld@gmail.com>
2015-03-02 20:34:01 -0700

    minor changes to 2

--- HW3-StarterCode/Problem2/ReportPass.java
+++ HW3-StarterCode/Problem2/ReportPass.java
@@ -1,5 +1,7 @@
 public class ReportPass implements Observer{
+	boolean testPass;
 	public void update(boolean reportSucceeds, boolean timeout){
+		this.testPass = reportSucceeds;
 		// if test status is true, return the report
 		if (reportSucceeds == true) {
 			System.out.println("Succeed: Autograder has succeeded");
--- HW3-StarterCode/Problem2/TimeOut.java
+++ HW3-StarterCode/Problem2/TimeOut.java
@@ -1,6 +1,10 @@
 public class TimeOut implements Observer{
+	private boolean passed;
+	private boolean timeout;
 	public void update(boolean reportSucceeds, boolean timeout){
 		// if test status is false, print LastErrorwasTimeout
+		this.passed = reportSucceeds;
+		this.timeout = timeout;
 		if (reportSucceeds == false) {
 			if (timeout == true) {
 				System.out.println("Failed: Autograder has timed out");
